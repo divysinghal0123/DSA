1a68322f77f818c2b5e793608533adcdca4b79b6
Divy Singhal <76985594+divysinghal0123@users.noreply.github.com>
2026-09-24 23:26:45 +0530

    Time: 49 ms (47.75%), Space: 29.2 MB (28.05%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,40 +4,34 @@ public:
         int n = nums.size();
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        int i = 0;
+        int i;
         for(i=0;i<n-2;i++){
-            
-            if(i > 0 && nums[i] == nums[i - 1]){
+            if(i > 0 && nums[i] == nums[i-1]){
                 continue;
             }
             int left = i + 1;
             int right = n - 1;
-
             while(left < right){
-                int sum = nums[i] + nums[right] + nums[left];
+                
+            int sum = nums[i] + nums[right] + nums[left];
+            if( sum == 0 ){
+                ans.push_back({nums[i],nums[right],nums[left]});
+                left++;
+                right--;
 
-                if(sum == 0){
-                    ans.push_back({nums[i],nums[right],nums[left]});
-                    left ++;
-                    right --;
-
-                    // Skip duplicate left values
-                    while(left < right && nums[left] == nums[left - 1]) {
-                        left++;
-                    }
-
-                    // Skip duplicate right values
-                    while(left < right && nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-                }else if( sum > 0 ){
-                    right --;
-                }else if( sum < 0){
-                    left ++;
+                while(left < right && nums[left] == nums[left-1]){
+                    left++;
                 }
+                while(left < right && nums[right] == nums[right + 1]){
+                    right--;
+                }
+            }else if( sum < 0 ){
+                left++;
+            }else if( sum > 0 ){
+                right--;
+            }
             }
         }
         return ans;
-        
     }
 };
