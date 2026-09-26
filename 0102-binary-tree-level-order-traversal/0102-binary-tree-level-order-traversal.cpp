5f8628c85a5e5963f21782a1cc64eab239f52b78
Divy Singhal <76985594+divysinghal0123@users.noreply.github.com>
2026-09-26 23:56:33 +0530

    Time: 0 ms (100%), Space: 17 MB (90.41%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,24 +12,23 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
+        vector<vector<int>> ans;  // Decalre the answer vector.
         if(root == NULL) return ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
+        queue<TreeNode*> q; //Decalre the queue.
+        q.push(root); //push the root inside thr queue.
+        while(!q.empty()){ //now after we will dot his until the queue gets empty.
             int n = q.size();
-            vector<int> level;
+            vector<int> store; //datastructure to store the node.
             int i;
-            for(i=0;i<n;i++){
-                TreeNode* node = q.front();
-                q.pop();
+            for(i=0;i<n;i++){ //we will traverse till the size of queue.
+                TreeNode* node = q.front(); // now we store the first node or front node to the node attribute.
+                q.pop(); // then we pop it for comparison.
                 if(node->left != NULL) q.push(node->left);
                 if(node->right != NULL) q.push(node->right);
-                level.push_back(node->val);
+                store.push_back(node->val); //push the root node.
             }
-            ans.push_back(level);
+            ans.push_back(store); // then store the nodes int the answer.
         }
         return ans;
-
     }
 };
